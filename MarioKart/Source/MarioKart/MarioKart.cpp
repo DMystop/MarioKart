d10d7e4ed4ec0673b940b358287fbfdccf450be7
DMystop <jouliogames@gmail.com>
2025-02-24 16:13:44 +0100

    Project Created

---
+++ MarioKart/Source/MarioKart/MarioKart.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MarioKart.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MarioKart, "MarioKart" );
